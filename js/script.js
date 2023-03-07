5d9553f74b876b7c5c31fe092f5b3e01886fcf64
Batyr <sergobatyr95@gmail.com>
2023-03-07 20:08:25 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+"use strict";
+
+    // const number = 4;
+
+    // console.log(number);
+
+    // let number1 = "id";
+
+    // for (let i = 0; i <= 4; i++) {
+    //     console.log(number1);
+    // }
+
+    // const obj = {
+    //     name: "John",
+    //     age: 22,
+    //     gender: "M"
+    // };
+
+    // console.log(obj["name"]);
+
+    // let arr = ["orange", "apple", 'sasyk', 'arbuz', 6, {}, ['sdadas', 3434] ];
+
+    // for(let i = 0; i < arr.length; i++) {
+    //    console.log(arr[i]);
+    // }
+
+
+    // let a = ["orange", "apple", 'sasyk', 'arbuz', 6, {}, ['sdadas', 3434] ];
+    // a.forEach((element) => {
+    //         console.log(element);
+    //     }
+    // );
+
+
+    // const result = confirm("Are u dalbaeb?");
+    // console.log(result);
+
+    // const answer = +prompt("How old are you?", "18");
+    // console.log(answer + 5);
+
+
+    // const answers = [];
+
+    // answers[0] = prompt('Whats your fname', '');
+    // answers[1] = prompt('Whats your sname', '');
+    // answers[2] = prompt('Whats your age', '');
+
+
+    // console.log(typeof(answers));
+    // console.log(typeof(null));
+
+// Интерполяция!!! //
+
+    // const category = 'toys';
+
+    // console.log(`https://google.com/${category}/5`);
+
+    // const user = "Babam";
+
+    // alert(`Hello, ${user}`);
+
+
+
+// console.log('arr' + ' - object');
+// console.log(4 + +"5");
+
+// let increment = 10,
+//     dicrement = 10;
+
+// increment++;
+// dicrement--;
+
+// console.log(increment++);
+// console.log(dicrement--);
+
+console.log(5%5);
+console.log(2**1 !== 4);
+
+let isUserIdhas = false,
+    isUserPassHas = false;
+
+console.log(isUserIdhas && !isUserPassHas);
